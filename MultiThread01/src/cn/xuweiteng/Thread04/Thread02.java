d0e51cd3491874380cad91656a9fb77d5d2d6f15
XuWeiteng <XuWeiteng@163.com>
2019-03-28 19:27:03 +0800

    利用Jconsole检测线程状态

---
+++ MultiThread01/src/cn/xuweiteng/Thread04/Thread02.java
@@ -0,0 +1,25 @@
+package cn.xuweiteng.Thread04;
+
+import java.util.concurrent.TimeUnit;
+
+public class Thread02 {
+
+	private final static Object THREAD = new Object();
+	
+	public void accessResource() {
+		synchronized (THREAD) {
+			try {
+				TimeUnit.MINUTES.sleep(10);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		final Thread02 thread = new Thread02();
+		for(int i=0; i<5; i++) {
+			new Thread(thread::accessResource).start();
+		}
+	}
+}
